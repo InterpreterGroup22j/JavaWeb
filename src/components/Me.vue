da03b3323ef042c1faaf434f52c6b4dcc752e23a
AmokuraD <1575823782@qq.com>
2019-04-26 17:10:30 +0800

    me

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -4,23 +4,22 @@
       <Input v-model="formItem.id" :disabled="true" placeholder="id" />
     </FormItem>
     <FormItem label="Username">
-      <Input v-model="formItem.username" :disabled="true" placeholder="username" />
-      <icon type="md-create"></icon>
+      <Input v-model="formItem.username" :disabled=write placeholder="username" />
     </FormItem>
     <FormItem label="Account">
       <Input v-model="formItem.account" :disabled="true" placeholder="account" />
     </FormItem>
-    <FormItem label="Password">
-      <Input v-model="formItem.password" :disabled="true" placeholder="password" />
-    </FormItem>
     <FormItem label="Sex">
-      <Input v-model="formItem.sex" :disabled="true" placeholder="sex" />
+      <Input v-model="formItem.sex" :disabled=write placeholder="sex" />
     </FormItem>
     <FormItem label="Height">
-      <Input v-model="formItem.height" :disabled="true" placeholder="height" />
+      <Input v-model="formItem.height" :disabled=write placeholder="height" />
     </FormItem>
     <FormItem label="Weight">
-      <Input v-model="formItem.weight" :disabled="true" placeholder="weight" />
+      <Input v-model="formItem.weight" :disabled=write placeholder="weight" />
+    </FormItem>
+    <FormItem>
+      <Button type="primary" :icon=type @click="fWrite()">{{btn_label}}</Button>
     </FormItem>
   </Form>
 </template>
@@ -33,36 +32,60 @@
           id: '',
           username: '',
           account: '',
-          password: '',
-          sex:'',
-          height:'',
-          weight:''
+          sex: '',
+          height: '',
+          weight: ''
+        },
+        write: true,
+        type: 'md-create',
+        btn_label: '编辑'
+      }
+    },
+    methods: {
+      fWrite() {
+        this.write = !this.write;
+        if (this.write) {
+          this.type = 'md-create';
+          this.btn_label = '编辑';
+          this.axios.post('http://h6be2u.natappfree.cc/user/UpdateUserInfo', {
+              user_name: this.formItem.username,
+              user_account: this.formItem.account,
+              user_sex: this.formItem.sex,
+              user_height: this.formItem.height,
+              user_weight: this.formItem.weight
+            })
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+        } else {
+          this.type = 'md-checkmark';
+          this.btn_label = '保存';
         }
       }
     },
-    mounted:{
-    info(){
-      this.axios.post('http://h6be2u.natappfree.cc/user/qryUserInfo', {
-            id: this.formItem.id,
-            user_name: this.formItem.username,
-            user_account: this.formItem.account,
-            user_password: this.formItem.password,
-            user_sex:this.formItem.sex,
-            user_height:this.formItem.height,
-            user_weight:this.formItem.weight
-          })
-          .then(res => console.log(res))
-          .catch(err => console.log(err));
-      }
-    }
+    // mounted:{
+    // info(){
+    //   this.axios.post('http://h6be2u.natappfree.cc/user/qryUserInfo', {
+    //         id: this.formItem.id,
+    //         user_name: this.formItem.username,
+    //         user_account: this.formItem.account,
+    //         user_password: this.formItem.password,
+    //         user_sex:this.formItem.sex,
+    //         user_height:this.formItem.height,
+    //         user_weight:this.formItem.weight
+    //       })
+    //       .then(res => console.log(res))
+    //       .catch(err => console.log(err));
+    //   }
+    // }
   }
 
 </script>
 <style>
-.MeForm{
-    width:400px;
-    margin: 0,auto;
-    padding:50px;
+  .MeForm {
+    width: 400px;
+    margin: 84px auto 0px auto;
+    padding: 50px;
     border: 1px solid black;
-}
+  }
+
 </style>
